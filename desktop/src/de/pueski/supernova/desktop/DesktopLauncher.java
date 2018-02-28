ceae920b45a8a7e558777923d1cdd2a2b9d969a9
mpue <matthias@pueski.de>
2018-02-28 20:19:26 +0100

    init

---
+++ desktop/src/de/pueski/supernova/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.pueski.supernova.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.pueski.supernova.SuperNOva;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperNOva(), config);
+	}
+}
